f69ad4f9404c825a481e653f54703d34c06ecdb4
Shyam Jethva <shyamjethva0205@gmail.com>
2026-02-22 19:26:58 +0530

    commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("✅ MongoDB Connected:", conn.connection.host);
+        console.log("✅ MongoDB Connected:", process.env.MONGO_URI);
     } catch (err) {
         console.error("❌ MongoDB connection error:", err.message);
         process.exit(1);
